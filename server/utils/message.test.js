af824ca955db559a8bd66bcd0262dbeb034794ec
Mohamed <mohamedsaadgadalhaq@gmail.com>
2019-03-19 21:22:01 +0200

    add geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () =>{
@@ -11,4 +11,18 @@ describe('generateMessage', () => {
       expect(message).toMatchObject({from, text});
 
     });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Admin';
+    var latitude = 1;
+    var longitude =1;
+    var url =`https://www.google.com/maps?q=${latitude},${longitude}`
+    var message = generateLocationMessage(from,latitude,longitude);
+    expect(message).toMatchObject({from,url});
+    expect(typeof message.createAt).toBe('number');
+
+
+  });
 });
